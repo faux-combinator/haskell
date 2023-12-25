7f14bd68378cc5470d3d2f2de816f9733b7a6aa3
vendethiel <vendethiel@hotmail.fr>
2023-12-25 01:44:49 +0100

    WIP FauxCombinator.hs

---
+++ src/FauxCombinator/Parser.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module FauxCombinator.Parser
+    ( peek
+    , expect
+    , runParserT
+    , runParser
+    ) where
+
+import Control.Applicative ((<|>))
+import Control.Monad (unless)
+import Control.Monad.Error.Class (MonadError, throwError)
+import Control.Monad.Except (ExceptT, runExceptT)
+import Control.Monad.State.Lazy (StateT(..), MonadState, get, put, evalStateT)
+import Data.Functor.Identity (Identity, runIdentity)
+
+data Token tt = Token { _tokenType :: tt, _tokenData :: String }
+
+data ParserError tt
+  = ParserErrorUnexpected tt tt -- (Actual, Expected)
+  | ParserErrorEitherNoMatch
+  | ParserErrorEOF
+
+data ParserData tt = ParserData
+  { _tokens :: [Token tt]
+  , _idx :: Int
+  }
+
+type ParserT tt m = StateT (ParserData tt) (ExceptT (ParserError tt) m)
+type Parser tt = ParserT tt Identity
+
+next :: (MonadState (ParserData tt) m, MonadError (ParserError tt) m) => m (Token tt)
+next = do -- TODO lenses? (idx <<+= 1)
+  state <- get
+  let idx = _idx state
+  if idx >= length (_tokens state)
+  then throwError ParserErrorEOF
+  else do
+    put state {_idx = idx + 1}
+    pure $ _tokens state !! idx
+
+peek :: (MonadState (ParserData tt) m) => m (Token tt)
+peek = do
+  state <- get
+  pure $ _tokens state !! _idx state
+
+expect :: (MonadState (ParserData tt) m, MonadError (ParserError tt) m, Eq tt) => tt -> m (Token tt)
+expect t = do
+  token <- next
+  unless (_tokenType token == t) $
+    throwError $ ParserErrorUnexpected (_tokenType token) t
+  pure token
+
+attempt :: (Monad m) => StateT tt m r -> StateT tt m (Maybe r)
+attempt act = (act >>= pure . Just) <|> pure Nothing
+
+--many :: (MonadState (ParserData tt) m) -> 
+
+runParserT :: (Monad m) => ParserT tt m r -> [Token tt] -> m (Either (ParserError tt) r)
+runParserT act tokens = runExceptT $ evalStateT act parser
+  where parser = ParserData { _tokens = tokens, _idx = 0 }
+
+runParser :: Parser tt r -> [Token tt] -> Either (ParserError tt) r
+runParser act tokens = runIdentity $ runParserT act tokens
